def7c9febec0990ee8f223e68d2a912366e0fd3e
Connor Brereton <breretonconnor@gmail.com>
2016-01-07 20:54:59 -0800

    Update and rename thermometer.c to thermometer.cpp

--- thermometer.cpp
+++ thermometer.cpp
@@ -1,4 +1,6 @@
+//analog pin
 const int sensorPin = A0;
+//initial temp
 const float baselineTemp = 20.0;
 
 void setup() {
@@ -25,30 +27,35 @@ void loop()
   float temperature = (voltage - .5) * 100;
   Serial.println(temperature);
 
+  //no change -- no lights
   if(temperature < baselineTemp)
   {
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
   }
+  //small change -- 1 light
   else if(temperature >= baselineTemp+2 && temperature < baselineTemp+4)
   {
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
   }
+  //medium change -- 2 lights
   else if(temperature >= baselineTemp+2 && temperature < baselineTemp+6)
   {
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, LOW);
   }
+  //lots of change -- 3 lights
   else if(temperature >=  baselineTemp+6)
   {
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
   }
+  //units are ms
   delay(50);
 
   
